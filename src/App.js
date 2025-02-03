fa67d3040ade8db38e40b5a05061fe1e1b16121b
Chaitanya <chaitu.raju@gmail.com>
2025-02-03 17:08:00 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -8,91 +8,100 @@ const App = () => {
   const [selState, setSelState] = useState("");
   const [selCity, setSelCity] = useState("");
   const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     fetch("https://crio-location-selector.onrender.com/countries")
       .then((res) => res.json())
       .then((data) => setCountries(data))
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
   }, []);
   useEffect(() => {
     if (!selCountry) return;
+
     setSelState("");
     setSelCity("");
+    setLoading(true);
     fetch(
       `https://crio-location-selector.onrender.com/country=${selCountry}/states`
     )
       .then((res) => res.json())
       .then((data) => setStates(data))
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
   }, [selCountry]);
   useEffect(() => {
     if (!selState) return;
     setSelCity("");
+    setLoading(true);
     fetch(
       `https://crio-location-selector.onrender.com/country=${selCountry}/state=${selState}/cities`
     )
       .then((res) => res.json())
       .then((data) => setCities(data))
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
   }, [selState]);
-
-  return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Select Location</h1>
-      <div
-        style={{
-          display: "flex",
-          gap: "20px",
-          padding: "10px",
-          justifyContent: "center",
-        }}
-      >
-        <select
-          value={selCountry}
-          onChange={(e) => setSelCountry(e.target.value)}
+  if (loading) return <div>Loading...</div>;
+  else
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>Select Location</h1>
+        <div
+          style={{
+            display: "flex",
+            gap: "20px",
+            padding: "10px",
+            justifyContent: "center",
+          }}
         >
-          <option>Select Country</option>
-          {countries.map((country) => (
-            <option value={country} key={country}>
-              {country}
-            </option>
-          ))}
-        </select>
-        <select
-          value={selState}
-          onChange={(e) => setSelState(e.target.value)}
-          disabled={selCountry ? false : true}
-        >
-          <option>Select State</option>
-          {states.map((state) => (
-            <option value={state} key={state}>
-              {state}
-            </option>
-          ))}
-        </select>
-        <select
-          value={selCity}
-          onChange={(e) => setSelCity(e.target.value)}
-          disabled={selState ? false : true}
-        >
-          <option>Select City</option>
-          {cities.map((city) => (
-            <option value={city} key={city}>
-              {city}
-            </option>
-          ))}
-        </select>
+          <select
+            value={selCountry}
+            onChange={(e) => setSelCountry(e.target.value)}
+          >
+            <option>Select Country</option>
+            {countries.map((country) => (
+              <option value={country} key={country}>
+                {country}
+              </option>
+            ))}
+          </select>
+          <select
+            value={selState}
+            onChange={(e) => setSelState(e.target.value)}
+            disabled={selCountry ? false : true}
+          >
+            <option>Select State</option>
+            {states.map((state) => (
+              <option value={state} key={state}>
+                {state}
+              </option>
+            ))}
+          </select>
+          <select
+            value={selCity}
+            onChange={(e) => setSelCity(e.target.value)}
+            disabled={selState ? false : true}
+          >
+            <option>Select City</option>
+            {cities.map((city) => (
+              <option value={city} key={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+        </div>
+        {selCity && (
+          <p style={{ fontWeight: "bold", textAlign: "center" }}>
+            You selected <span style={{ fontSize: "25px" }}>{selCity}, </span>
+            <span style={{ color: "gray" }}>
+              {selState}, {selCountry}
+            </span>
+          </p>
+        )}
       </div>
-      {selCity && (
-        <p style={{ fontWeight: "bold", textAlign: "center" }}>
-          You selected <span style={{ fontSize: "25px" }}>{selCity}, </span>
-          <span style={{ color: "gray" }}>
-            {selState}, {selCountry}
-          </span>
-        </p>
-      )}
-    </div>
-  );
+    );
 };
 
 export default App;
